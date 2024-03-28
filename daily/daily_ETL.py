b476b04ebdf9ffe0a8486e7f839f804ba87c52bd
Tianhao Wu <wthtony666@gmail.com>
2024-03-28 15:46:11 -0700

    correct data

--- daily/daily_ETL.py
+++ daily/daily_ETL.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 df = pd.read_csv('climate-daily.csv')
-print(df.columns)
+print(df['STATION_NAME'].unique().shape)
 df1 = pd.read_csv('climate-stations.csv')
 df1 = df1[['STATION_NAME','ELEVATION']]
-print(df1.columns)
 
 merged_df = pd.merge(df, df1, on='STATION_NAME', how='left')
+print(merged_df['STATION_NAME'].unique().shape)
 merged_df.to_csv('metro_vancouver_climate.csv', index=False)
